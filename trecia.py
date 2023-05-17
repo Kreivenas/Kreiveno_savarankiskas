acb3aeba88d46f9a6f0686aa88e4e1415056db72
kreivenas <kreivenastomas78@gmail.com>
2023-05-17 19:02:41 +0300

    .

--- trecia.py
+++ trecia.py
@@ -12,24 +12,24 @@ def sukurti_darbuotoja(vardas, amzius, pozicija):
     darbuotojas = {"vardas": vardas, "amzius": amzius, "pozicija": pozicija}
     return darbuotojas
 
-def issaugoti_darbuotojus(darbuotojai, failo_pavadinimas):
+def issaugoti_darbuotojus(Darbuotojai, failo_pavadinimas):
     with open(failo_pavadinimas, 'wb') as f:
-        pickle.dump(darbuotojai, f)
+        pickle.dump(Darbuotojai, f)
 
 def uzkrauti_darbuotojus(failo_pavadinimas):
     with open(failo_pavadinimas, 'rb') as f:
-        darbuotojai = pickle.load(f)
-    return darbuotojai
+        Darbuotojai = pickle.load(f)
+    return Darbuotojai
 
 darbuotojas1 = sukurti_darbuotoja("Jonas", 30, "Vadybininkas")
 darbuotojas2 = sukurti_darbuotoja("Petras", 35, "Programuotojas")
 darbuotojas3 = sukurti_darbuotoja("Linas", 28, "Dizaineris")
 
-darbuotojai = [darbuotojas1, darbuotojas2, darbuotojas3]
+Darbuotojai = [darbuotojas1, darbuotojas2, darbuotojas3]
 
-issaugoti_darbuotojus(darbuotojai, "darbuotojai.pickle")
+issaugoti_darbuotojus(Darbuotojai, "Darbuotojai.pickle")
 
-uzkrauti_darbuotojus = uzkrauti_darbuotojus("darbuotojai.pickle")
+uzkrauti_darbuotojus = uzkrauti_darbuotojus("Darbuotojai.pickle")
 
 for darbuotojas in uzkrauti_darbuotojus:
     print(darbuotojas["vardas"], darbuotojas["amzius"], darbuotojas["pozicija"])
